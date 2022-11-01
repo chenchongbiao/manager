e2b4ca0c525e33c20bdaf5dfc3aa47ca3c8592a9
bluesky <379865549@qq.com>
2022-11-01 20:48:22 +0800

    添加首页的状态栏

--- homepagestatus.cpp
+++ homepagestatus.cpp
@@ -1,7 +1,18 @@
+#include <QGraphicsOpacityEffect>
+#include <QtConcurrent>
+#include <QTimer>
+#include <QProcess>
+#include <sstream>
+#include <math.h>
+#include <unistd.h>
+
 #include "homepagestatus.h"
 #include "ui_homepagestatus.h"
+#include "waterprogress.h"
+#include "utils.h"
 
-#include <QGraphicsOpacityEffect>
+#define MB (1024 * 1024)
+#define KB (1024)
 
 HomePageStatus::HomePageStatus(QWidget *parent) :
     QWidget(parent),
@@ -9,6 +20,18 @@ HomePageStatus::HomePageStatus(QWidget *parent) :
 {
     ui->setupUi(this);
     initUI();
+
+    QTimer *timer = new QTimer(this);
+    timer->start(1000);
+    connect(timer, SIGNAL(timeout()), this, SLOT(updateWaterProgress()));
+
+    connect(loadStatusWp->getTimer(), SIGNAL(timeout()), this, SLOT(computerLoad()));  // 计算负载均衡状态
+    connect(cpuStatusWp->getTimer(), SIGNAL(timeout()), this, SLOT(getCPU()));
+    connect(memStatusWp->getTimer(), SIGNAL(timeout()), this, SLOT(getMemory()));
+    connect(diskStatusWp->getTimer(), SIGNAL(timeout()), this, SLOT(getDisk()));
+
+    process = new QProcess(this);
+    connect(process, SIGNAL(readyRead()), this, SLOT(ReadData()));
 }
 
 HomePageStatus::~HomePageStatus()
@@ -23,4 +46,126 @@ void HomePageStatus::initUI()
     QGraphicsOpacityEffect *opacityEffect_1 = new QGraphicsOpacityEffect;
     opacityEffect_1->setOpacity(0.1);
     ui->line1_widget->setGraphicsEffect(opacityEffect_1);
+
+    cpuNum = Utils::exec("cpufreq-info | grep 'analyzing CPU'").split("\n").size();  // CPU个数固定，不重复计算)
+    ui->cpu_core_num->setText(QString("%1核心").arg(cpuNum));
+    loadStatusWp = new WaterProgress(ui->load_status,900);
+    cpuStatusWp = new WaterProgress(ui->cpu_status,900);
+    memStatusWp = new WaterProgress(ui->memory_status,300);
+    diskStatusWp = new WaterProgress(ui->disk_status,1000);
+    updateWaterProgress();
+}
+
+void HomePageStatus::updateWaterProgress()
+{
+    loadStatusWp->setValue(loadPercent);  // 修改负载状态
+    ui->load_status_info->setText(loadMsg);
+    cpuStatusWp->setValue(cpuPercent);  // 修改CPU状态
+    memStatusWp->setValue(memoryPercent);  // 修改内存状态
+    ui->memory_space->setText(memoryMsg);
+    diskStatusWp->setValue(diskPercent);  // 修改硬盘状态
+    ui->disk_space->setText(diskyMsg);
+}
+
+void HomePageStatus::getCPU()
+{
+    if (process->state() == QProcess::NotRunning) {
+        totalNew = idleNew = 0;
+        process->start(QString("cat %1").arg(cpuFile));
+    }
+}
+
+void HomePageStatus::getMemory()
+{
+    if (process->state() == QProcess::NotRunning) {
+        process->start(QString("cat %1").arg(memFile));
+    }
+}
+
+void HomePageStatus::getDisk()
+{
+    if (process->state() == QProcess::NotRunning) {
+        process->start("df -h");
+    }
+}
+
+void HomePageStatus::computerLoad()
+{
+    load = Utils::exec("uptime" + Utils::awk("average: ","2") + Utils::awk(",","1")).toFloat();  // 获取负载均衡数据
+    // 算法来自宝塔，因为水球的数值为整形，最后做了转换
+    loadPercent = (int)round(load / (cpuNum * 2) * 100);
+    if (loadPercent > 100)
+    {
+        loadPercent = 100;
+    }
+    if (loadPercent <= 30)
+    {
+        loadMsg = "运行流畅";
+    }
+    if (loadPercent > 30 && loadPercent <= 70)
+    {
+        loadMsg = "运行正常";
+    }
+    if (loadPercent > 70 && loadPercent <= 90)
+    {
+        loadMsg = "运行缓慢";
+    }
+    if (loadPercent > 90)
+    {
+        loadMsg = "运行堵塞";
+    }
+}
+
+void HomePageStatus::ReadData()
+{
+    QtConcurrent::run([=](){
+        while (!process->atEnd()) {
+            QString s = QLatin1String(process->readLine());
+            if (s.startsWith("cpu")) {
+                QStringList list = s.split(" ");
+                idleNew = list.at(5).toInt();
+                foreach (QString value, list) {
+                    totalNew += value.toInt();
+                }
+                int total = totalNew - totalOld;
+                int idle = idleNew - idleOld;
+                cpuPercent = 100 * (total - idle) / total;
+                totalOld = totalNew;
+                idleOld = idleNew;
+                break;
+            } else if (s.startsWith("MemTotal")) {
+                s = s.replace(" ", "");
+                s = s.split(":").at(1);
+                memoryAll = s.left(s.length() - 3).toInt() / KB;
+            } else if (s.startsWith("MemFree")) {
+                s = s.replace(" ", "");
+                s = s.split(":").at(1);
+                memoryFree = s.left(s.length() - 3).toInt() / KB;
+    //            qDebug() << "MemFree " << memoryFree << endl;
+            } else if (s.startsWith("Buffers")) {
+                s = s.replace(" ", "");
+                s = s.split(":").at(1);
+                memoryFree += s.left(s.length() - 3).toInt() / KB;
+    //            qDebug() << "Buffers " << memoryFree << endl;
+            } else if (s.startsWith("Cached")) {
+                s = s.replace(" ", "");
+                s = s.split(":").at(1);
+                memoryFree += s.left(s.length() - 3).toInt() / KB;
+                memoryUse = memoryAll - memoryFree;
+                memoryPercent = 100 * memoryUse / memoryAll;
+                memoryMsg = QString("%1MB/%2MB").arg(memoryUse).arg(memoryAll);
+//                qDebug() << s;
+                break;
+            } else if (s.startsWith("/dev/sda") | s.startsWith("/dev/nvme")) {
+                s = s.replace(QRegExp("[\\s]+"), " ");
+                QStringList list = s.split(" ");
+                diskUse = list.at(2).left(list.at(2).length() - 1).toInt();
+                diskAll = list.at(1).left(list.at(1).length() - 1).toInt();
+                diskPercent = list.at(4).left(list.at(4).length() - 1).toInt();
+                diskyMsg = QString("%1/%2").arg(list.at(2)).arg(list.at(1));
+//                qDebug() << QString("diskUse %1 diskAll %2 diskPercent %3").arg(diskUse).arg(diskAll).arg(diskPercent);
+                break;
+            }
+        }
+    });
 }
--- homepagestatus.h
+++ homepagestatus.h
@@ -2,6 +2,9 @@
 #define HOMEPAGESTATUS_H
 
 #include <QWidget>
+#include <QProcess>
+
+#include "waterprogress.h"
 
 namespace Ui {
 class HomePageStatus;
@@ -17,9 +20,47 @@ public:
 
 private:
     void initUI();  // 初始化界面
+private slots:
+    void updateWaterProgress(); // 更新进度条
+    void computerLoad();         // 计算负载状态
+    void getCPU();     // 读取/proc/stat数据
+    void getMemory();  // 读取/proc/mominfo
+    void ReadData();   // 读取内存和CPU数据
+    void getDisk();    // 读取磁盘数据
 
 private:
     Ui::HomePageStatus *ui;
+    WaterProgress *loadStatusWp;  // 负载状态的进度条
+    WaterProgress *cpuStatusWp;   // CPU使用率的进度条
+    WaterProgress *memStatusWp;   // 内存使用率进度条
+    WaterProgress *diskStatusWp;  // 磁盘使用率进度条
+    QString cpuFile="/proc/stat";
+    QString memFile="/proc/meminfo";
+    QString issueFile="/etc/issue";
+
+    float load = 0; // 负载均衡的值
+    int cpuNum = 0;  // cpu数量
+    int memNum = 0;  // 内存大小
+    int loadPercent = 0; // 负载状态的百分比
+    QString loadMsg; // 伏在状态下的信息
+
+    int totalNew=0, idleNew=0, totalOld=0, idleOld=0;
+    int cpuPercent=0;  // cpu使用率
+    int memoryPercent=0;// 内存使用率
+    int memoryAll=0;  // 内存总数
+    int memoryUse=0;  //  内存使用空间
+    int memoryFree=0;  // 空闲内存数
+    QString memoryMsg; // 内存使用率下的信息
+
+    int diskPercent=0; // 磁盘使用率
+    int diskUse=0;  //  磁盘使用空间
+    int diskAll=0;  // 磁盘总量
+    QString diskyMsg; // 磁盘使用率下的信息
+
+    QString issue;  // 发行版
+    QString ip; // ip地址
+    QString  architecture;  //架构
+    QProcess *process;
 };
 
 #endif // HOMEPAGESTATUS_H
