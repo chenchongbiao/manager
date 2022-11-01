bc83a7ab380b3211e4de20b27b66d59e8ac6b6f0
bluesky <379865549@qq.com>
2022-11-02 01:41:32 +0800

    界面修改

--- dockerpage.cpp
+++ dockerpage.cpp
@@ -1,7 +1,7 @@
 #include <QPushButton>
 #include <QVBoxLayout>
 #include <QGraphicsOpacityEffect>
-#include <QListWidgetItem>
+#include <DFrame>
 
 #include "dockerpage.h"
 #include "ui_dockerpage.h"
@@ -21,16 +21,12 @@ DockerPage::~DockerPage()
 
 void DockerPage::initUI()
 {
-    QPushButton *btn = new QPushButton("测试");  // 主页软件单条数据控件
-    btn->resize(100,50);
-    QListWidgetItem *WContainerItem=new QListWidgetItem(ui->listWidget);
-    WContainerItem->setSizeHint(QSize(40,40));
-//        WContainerItem->setToolTip(); // 提示框
-//    WContainerItem->setFlags(Qt::ItemIsSelectable); // 取消选择项
-    ui->listWidget->setItemWidget(WContainerItem,btn);  // 将homePageSoftItem赋予WContainerItem
-//    QVBoxLayout *pVBoxLayout = new QVBoxLayout(ui->menuDfrm);
-//    pVBoxLayout->setMargin(0);
-//    pVBoxLayout->setSpacing(0);
+
+    QVBoxLayout *pVBoxLayout = new QVBoxLayout(ui->menu_widget);
+    pVBoxLayout->setMargin(0);
+    pVBoxLayout->setSpacing(0);
+
+
 
 //    QGraphicsOpacityEffect *opacityEffect = new QGraphicsOpacityEffect;
 //    opacityEffect->setOpacity(0.6);
