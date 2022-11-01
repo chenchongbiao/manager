9dca8126fdb881947ae9587ac57532850599f7e5
bluesky <379865549@qq.com>
2022-11-01 19:12:23 +0800

    添加软件介绍

--- main.cpp
+++ main.cpp
@@ -7,9 +7,19 @@
 int main(int argc, char *argv[])
 {
     DApplication a(argc, argv);
-    a.loadTranslator();  // 载入翻译
+    a.setOrganizationName("开发环境管理工具");
+    a.setApplicationName("dev-manager");
+    a.setApplicationVersion("1.0");
+//    a.setProductIcon(QIcon(":/images/logo.svg"));
+    a.setProductName("Dev-Manager");
+    a.setApplicationDescription("这是一个集成环境软件，将开发环境做整合管理");
 
-//    DApplicationSettings settings;  // 保存窗口主题设置
+    a.loadTranslator();  // 载入翻译
+    a.setApplicationDisplayName(QCoreApplication::translate("Main", "DTK Application"));
+    // 保存程序的窗口主题设置
+    DApplicationSettings as;
+    Q_UNUSED(as)
+
     Widget w;
     w.show();
     Dtk::Widget::moveToCenter(&w);
