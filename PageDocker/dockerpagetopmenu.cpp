56e534d7c70a88eead2f8b7767dc9c96638d345f
bluesky <379865549@qq.com>
2022-11-02 23:31:24 +0800

    Docker页面顶部菜单

---
+++ PageDocker/dockerpagetopmenu.cpp
@@ -0,0 +1,89 @@
+#include <QDebug>
+
+#include "dockerpagetopmenu.h"
+
+DockerPageTopMenu::DockerPageTopMenu(QWidget *parent) : QWidget(parent)
+{
+    initUI(parent);
+}
+
+void DockerPageTopMenu::initUI(QWidget *parent)
+{
+    int leftMenuBtnWidth = 50;
+    int leftMenuBtnheight = parent->height();
+
+    QFont font;
+    font.setFamily(QString::fromUtf8("Zeniq"));
+    font.setPointSize(11);
+    font.setBold(false);
+    font.setItalic(false);
+    font.setWeight(50);
+
+    topMenuWidget = new QWidget(parent);
+    topMenuWidget->resize(parent->width(),parent->height());
+
+    topMenuWidgetLayout = new QHBoxLayout(topMenuWidget);
+    topMenuWidgetLayout->setSpacing(6);
+    topMenuWidgetLayout->setContentsMargins(0, 0, 0, 0);
+//    topMenuWidgetLayout->insertSpacing(0, -12);
+
+    container = new QPushButton(topMenuWidget);
+    container->setText("容器");
+    topMenuWidgetLayout->addWidget(container);
+
+    images = new QPushButton(topMenuWidget);
+    images->setText("镜像");
+    topMenuWidgetLayout->addWidget(images);
+
+    net = new QPushButton(topMenuWidget);
+    net->setText("网络");
+    topMenuWidgetLayout->addWidget(net);
+
+    volume = new QPushButton(topMenuWidget);
+    volume->setText("存储卷");
+    topMenuWidgetLayout->addWidget(volume);
+
+    wareHouse = new QPushButton(topMenuWidget);
+    wareHouse->setText("仓库");
+    topMenuWidgetLayout->addWidget(wareHouse);
+
+    setting = new QPushButton(topMenuWidget);
+    setting->setText("设置");
+    topMenuWidgetLayout->addWidget(setting);
+
+    topMenuList << container
+                << images
+                << net
+                << volume
+                << wareHouse
+                << setting;
+
+    for (int i=0;i < topMenuList.count();i++)
+    {
+        QPushButton *btn = topMenuList.at(i);
+        btn->setFont(font);
+        btn->setFocusPolicy(Qt::NoFocus);  // 去掉虚线框
+        checkMenu(btn,false);
+
+        connect(btn, &QPushButton::clicked, this, [=](){
+            QPushButton *curBtn = (QPushButton *)sender();  // 槽函数中调用sender函数，返回指向发送信号的对象的指针
+            qDebug() << "当前按下" << curBtn->text();
+            for (int i = 0; i < topMenuList.count(); i++)
+            {
+                QPushButton *btn = topMenuList.at(i);
+                checkMenu(btn,curBtn == btn);
+            }
+        });
+    }
+    checkMenu(container,true);  // 默认选中容器
+}
+
+void DockerPageTopMenu::checkMenu(QPushButton *btn,bool isCheck)
+{
+    if (isCheck)
+    {
+        btn->setStyleSheet("color: #FFFFFF;  background-color: #0081ff; border-radius: 5; border: 0px; height: 45px;");
+    } else {
+        btn->setStyleSheet("color: #252525; border-radius: 5; border: 0px; height: 45px;");
+    }
+}
---
+++ PageDocker/dockerpagetopmenu.h
@@ -0,0 +1,32 @@
+#ifndef DOCKERPAGETOPMENU_H
+#define DOCKERPAGETOPMENU_H
+
+#include <QWidget>
+#include <QPushButton>
+#include <QHBoxLayout>
+
+class DockerPageTopMenu : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit DockerPageTopMenu(QWidget *parent = nullptr);
+
+    void checkMenu(QPushButton *btn,bool isCheck);
+    void chooseLeftMenu(int index);          // 菜单切换
+
+private:
+    void initUI(QWidget *parent = nullptr);
+
+private:
+    QWidget *topMenuWidget;
+    QList<QPushButton *> topMenuList;        // 左侧菜单列表
+    QPushButton *container;                  // 容器
+    QPushButton *images;                     // 镜像
+    QPushButton *net;                        // 网络
+    QPushButton *volume;                     // 存储卷
+    QPushButton *wareHouse;                  // 仓库
+    QPushButton *setting;                    // 设置
+    QHBoxLayout *topMenuWidgetLayout;        // 垂直布局
+};
+
+#endif // DOCKERPAGETOPMENU_H
