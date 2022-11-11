e80fa2dfefe15b54720b1a34ce33f9e316e187c6
bluesky <379865549@qq.com>
2022-11-11 17:27:11 +0800

    container模型，对应数据表的字段，用来存储数据

---
+++ model/containermodel.h
@@ -0,0 +1,34 @@
+#include <QString>
+
+#ifndef CONTAINERMODEL_H
+#define CONTAINERMODEL_H
+
+
+class ContainerModel
+{
+public:
+    ContainerModel();
+    ContainerModel(QString ContainerId,QString Name,QString State,QString Image,QString Port,QString Command);
+    QString GetContainerId();
+    void SetContainerId(QString ContainerId);
+    QString GetName();
+    void SetName(QString Name);
+    QString GetState();
+    void SetState(QString State);
+    QString GetImage();
+    void SetImage(QString Image);
+    QString GetPort();
+    void SetPort(QString Port);
+    QString GetCommand();
+    void SetCommand(QString Command);
+
+private:
+    QString ContainerId;  // 容器id
+    QString Name;         // 容器名
+    QString State;        // 容器运行状态
+    QString Image;        // 使用的镜像
+    QString Port;         // 暴露端口
+    QString Command;      // 容器执行指令
+};
+
+#endif // CONTAINER_H
