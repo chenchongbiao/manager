5b7a1b3ff015dd639b1a27ac71ee95d61b39ec3b
bluesky <379865549@qq.com>
2022-11-14 16:38:20 +0800

    添加checkRadioBtnList表示被选中的数据

--- PageDocker/container.h
+++ PageDocker/container.h
@@ -29,26 +29,27 @@ public:
 
 private:
     Ui::Container *ui;
-    QWidget *conBtnWidget;               // 容器界面的操作按钮
-    QWidget *columnWidget;               // 列名
-    QWidget *dockerList;                 // 容器列表
-    QHBoxLayout *conBtnLayout;           // 按钮的布局
-    QHBoxLayout *columnLayout;           // 列名的布局
-    DPushButton *startBtn;               // 启动容器
-    DPushButton *stopBtn;                // 停止容器
-    DPushButton *restartBtn;             // 重启容器
-    DPushButton *deleteBtn;              // 删除容器
-    DPushButton *createBtn;              // 创建容器
-    QRadioButton *checkAllBtn;           // 全选按钮
-    DLabel *name;                        // 容器名称
-    DLabel *status;                      // 容器状态
-    DLabel *address;                     // 容器监控
-    DLabel *image;                       // 容器镜像
-    DLabel *port;                        // 容器端口
-    DLabel *contaierId;                  // 容器id
-    QSqlDatabase db;                     // 数据库
-    ContainerMapper containerMapper;     // container数据表操作
-    QByteArray contaierArray;            // 从sessionbus中获取到的容器数据
+    QWidget *conBtnWidget;                     // 容器界面的操作按钮
+    QWidget *columnWidget;                     // 列名
+    QWidget *dockerList;                       // 容器列表
+    QHBoxLayout *conBtnLayout;                 // 按钮的布局
+    QHBoxLayout *columnLayout;                 // 列名的布局
+    DPushButton *startBtn;                     // 启动容器
+    DPushButton *stopBtn;                      // 停止容器
+    DPushButton *restartBtn;                   // 重启容器
+    DPushButton *deleteBtn;                    // 删除容器
+    DPushButton *createBtn;                    // 创建容器
+    QRadioButton *checkAllBtn;                 // 全选按钮
+    DLabel *name;                              // 容器名称
+    DLabel *status;                            // 容器状态
+    DLabel *address;                           // 容器监控
+    DLabel *image;                             // 容器镜像
+    DLabel *port;                              // 容器端口
+    DLabel *contaierId;                        // 容器id
+    QSqlDatabase db;                           // 数据库
+    ContainerMapper containerMapper;           // container数据表操作
+    QByteArray contaierArray;                  // 从sessionbus中获取到的容器数据
+    QList<QRadioButton *> checkRadioBtnList;  // 被选中的容器数据
 };
 
 #endif // CONTAINER_H
