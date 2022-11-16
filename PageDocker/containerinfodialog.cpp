c1ae6756cc35834b7a69a1763f961302470fccea
bluesky <379865549@qq.com>
2022-11-16 15:15:16 +0800

    添加弹窗顶部切换按钮

--- PageDocker/containerinfodialog.cpp
+++ PageDocker/containerinfodialog.cpp
@@ -1,3 +1,5 @@
+#include <DPushButton>
+
 #include "containerinfodialog.h"
 #include "ui_containerinfodialog.h"
 
@@ -6,9 +8,73 @@ ContainerInfoDialog::ContainerInfoDialog(QWidget *parent) :
     ui(new Ui::ContainerInfoDialog)
 {
     ui->setupUi(this);
+    initUI();
+
+    connect(basicBtn, &DPushButton::clicked, this, [=](){ContainerInfoDialog::chooseLeftMenu(0);});
+    connect(netBtn, &DPushButton::clicked, this, [=](){ContainerInfoDialog::chooseLeftMenu(1);});
+    connect(volumeBtn, &DPushButton::clicked, this, [=](){ContainerInfoDialog::chooseLeftMenu(2);});
 }
 
 ContainerInfoDialog::~ContainerInfoDialog()
 {
     delete ui;
 }
+
+void ContainerInfoDialog::initUI()
+{
+    topBtnWdg = new QWidget(ui->topBtnFrm);
+    topBtnWdgLayout = new QHBoxLayout(topBtnWdg);
+    topBtnWdgLayout->setSpacing(6);
+    topBtnWdgLayout->setContentsMargins(0, 0, 0, 0);
+
+    basicBtn = new DPushButton("基本信息");
+    basicBtn->setStyleSheet("color: #252525; border-radius: 5; border: 0px;");
+    basicBtn->setFixedSize(80,ui->topBtnFrm->height());
+    topBtnWdgLayout->addWidget(basicBtn);
+
+    netBtn = new QPushButton("网络信息");
+    netBtn->setFixedSize(80,ui->topBtnFrm->height());
+    topBtnWdgLayout->addWidget(netBtn);
+
+    volumeBtn = new QPushButton("存储信息");
+    volumeBtn->setFixedSize(80,ui->topBtnFrm->height());
+    topBtnWdgLayout->addWidget(volumeBtn);
+
+    topBtnList << basicBtn
+               << netBtn
+               << volumeBtn;
+
+    for(int i=0;i < topBtnList.count();i++) {
+        DPushButton *btn = topBtnList.at(i);
+        checkMenu(btn,false);
+
+        connect(btn,&DPushButton::clicked,this,[=](){
+           DPushButton *curBtn = (DPushButton *) sender();
+           qDebug() << "当前按下" << curBtn->text();
+           for(int i=0;i < topBtnList.count();i++) {
+               DPushButton *btn = topBtnList.at(i);
+               checkMenu(btn,curBtn==btn);
+           }
+        });
+    }
+
+    checkMenu(basicBtn,true);  // 默认选中基本信息按钮
+}
+
+void ContainerInfoDialog::checkMenu(QPushButton *btn,bool isCheck)
+{
+    if (isCheck)
+    {
+        btn->setStyleSheet("color: #409EFF; border-radius: 5; border: 1px solid rgb(64,158,255); font-size:15px;");
+    } else {
+        btn->setStyleSheet("color: #252525; border-radius: 5; border: 0px;");
+    }
+}
+
+void ContainerInfoDialog::chooseLeftMenu(int index)  // 菜单切换逻辑
+{
+    nowMenu = index;
+    ui->stackedWidget->setCurrentIndex(nowMenu);
+}
+
+
--- PageDocker/containerinfodialog.h
+++ PageDocker/containerinfodialog.h
@@ -2,6 +2,10 @@
 #define CONTAINERINFODIALOG_H
 
 #include <QWidget>
+#include <QHBoxLayout>
+#include <DPushButton>
+
+DWIDGET_USE_NAMESPACE
 
 namespace Ui {
 class ContainerInfoDialog;
@@ -14,12 +18,21 @@ class ContainerInfoDialog : public QWidget
 public:
     explicit ContainerInfoDialog(QWidget *parent = nullptr);
     ~ContainerInfoDialog();
+    void checkMenu(QPushButton *btn, bool isCheck);
+    void chooseLeftMenu(int index);
 
 private:
     void initUI();
 
 private:
     Ui::ContainerInfoDialog *ui;
+    QWidget *topBtnWdg;                  // 顶部按钮的widget
+    QHBoxLayout *topBtnWdgLayout;        // 顶部按钮水平布局
+    DPushButton *basicBtn;               // 基本信息按钮
+    DPushButton *netBtn;                 // 网络信息按钮
+    DPushButton *volumeBtn;              // 存储信息按钮
+    QList<DPushButton *> topBtnList;     // 顶部按钮列表
+    int nowMenu;                         // 当前菜单
 };
 
 #endif // CONTAINERINFODIALOG_H
