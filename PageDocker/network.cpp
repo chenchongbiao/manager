e03d88e7162522fa2f8b5adf0b67b34402bb158d
bluesky <379865549@qq.com>
2023-01-08 21:54:14 +0800

    添加搜索栏和按钮的部分

--- PageDocker/network.cpp
+++ PageDocker/network.cpp
@@ -16,5 +16,34 @@ Network::~Network()
 
 void Network::initUI()
 {
+    // 通过界面
+    mlist = new MListWidget(this);
+    /*
+        操作栏
+    */
+    netBtnWidget = new QWidget(mlist->getBtnDrm());
+    netBtnWidget->resize(mlist->getBtnDrm()->width(),mlist->getBtnDrm()->height());
+    netBtnLayout = new QHBoxLayout(netBtnWidget);
+    netBtnLayout->setSpacing(6); // 部件之间的间距
+    netBtnLayout->setMargin(0);  // 设置上下左右的间距
+    netBtnLayout->setAlignment(Qt::AlignLeft);
 
+    searchEdit = new DLineEdit();
+    searchEdit->setFixedWidth(200);
+    searchEdit->setPlaceholderText("请输入容器名");
+    netBtnLayout->addWidget(searchEdit);
+
+    searchBtn = new DPushButton("搜索");
+    searchBtn->setFixedSize(60,35);
+    searchBtn->setStyleSheet("color: #FFFFFF; background-color: #67C23A; border-radius: 5; width: 60px; font-size:15px;");
+    connect(searchBtn, &QPushButton::clicked, this, &Network::SearchContainer);
+    netBtnLayout->addWidget(searchBtn);
+
+
+}
+
+void Network::SearchContainer()
+{
+    qDebug() << "搜索网络按钮被点击";
+    QString keyword = searchEdit->text();
 }
--- PageDocker/network.h
+++ PageDocker/network.h
@@ -2,6 +2,12 @@
 #define NETWORK_H
 
 #include <QWidget>
+#include <QHBoxLayout>
+#include <DLineEdit>
+#include <DPushButton>
+#include <QDebug>
+
+#include "mlistwidget.h"
 
 //DWIDGET_USE_NAMESPACE
 //namespace Ui {
@@ -17,8 +23,17 @@ public:
     ~Network();
     void initUI();
 
+public slots:
+    void SearchContainer();                   // 搜索网络的点击事件
+
 private:
 //    Ui::Network *ui;
+    QWidget *netBtnWidget;                    // 网络界面的操作按钮widget
+    QHBoxLayout *netBtnLayout;                // 按钮的布局
+    MListWidget *mlist;                       // 通用界面的组件
+    DLineEdit *searchEdit;                    // 搜索框
+    DPushButton *searchBtn;                   // 搜索按钮
+    QByteArray containerArray;                // 从sessionbus中获取到的网络数据
 };
 
 #endif // NETWORK_H
