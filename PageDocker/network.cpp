4c0f876fd2c9d912c862500ce94c2e5053e733a9
bluesky <379865549@qq.com>
2023-01-30 17:51:22 +0800

    修改生成网络列表的函数

--- PageDocker/network.cpp
+++ PageDocker/network.cpp
@@ -99,97 +99,103 @@ void Network::initNetworkListUI()
     QJsonParseError jsonError;
     QJsonDocument document = QJsonDocument::fromJson(networkArray, &jsonError);  // 转化为 JSON 文档
     if (!document.isNull() && (jsonError.error == QJsonParseError::NoError)) { // 解析未发生错误
-        if (document.isArray()) { // JSON 文档为数组
-            QJsonArray networkJson = document.array();
-            int netSize = networkJson.size();
-            for (int i = 0; i < netSize; i++) {
-                QJsonValue value = networkJson.at(i);      // 取出单个json
-                QJsonObject obj = value.toObject();        // 转换为object
+        if (document.isObject()) {  // JSON 文档为对象
+            QJsonObject object = document.object();  // 转化为对象
+            if (object.contains("data")) {  // 包含指定的 key
+                QJsonValue dataValue = object.value("data");  // 获取指定 key 对应的 value
+                if (dataValue.isArray()) { // JSON 文档为数组
+                    QJsonArray networkJson = dataValue.toArray();
+                    int netSize = networkJson.size();
+                    for (int i = 0; i < netSize; i++) {
+                        QJsonValue value = networkJson.at(i);      // 取出单个json
+                        QJsonObject obj = value.toObject();        // 转换为object
 
-                QWidget *networkWdg = new QWidget(mlist->getListWidget());
-                QHBoxLayout *layout = new QHBoxLayout(networkWdg);
-                layout->setMargin(0);  // 设置外边距
+                        QWidget *networkWdg = new QWidget(mlist->getListWidget());
+                        QHBoxLayout *layout = new QHBoxLayout(networkWdg);
+                        layout->setMargin(0);  // 设置外边距
 
-                QCheckBox *checkBtn = new QCheckBox(mlist->getListWidget());
-                checkBtn->setFixedSize(mlist->getBtnDrm()->height()-20,mlist->getBtnDrm()->height());
-                layout->addWidget(checkBtn);
-                connect(checkBtn,&QCheckBox::clicked, this, &Network::CheckNetwork);
+                        QCheckBox *checkBtn = new QCheckBox(mlist->getListWidget());
+                        checkBtn->setFixedSize(mlist->getBtnDrm()->height()-20,mlist->getBtnDrm()->height());
+                        layout->addWidget(checkBtn);
+                        connect(checkBtn,&QCheckBox::clicked, this, &Network::CheckNetwork);
 
-                // 定义一个函数，用来添加表头，依次传入，表头名称，以及宽度
-                auto addNetworkItem = [&](const QString labName, const int labWidth){
-                    DLabel *label = new DLabel(labName);
-                    label->setAlignment(Qt::AlignCenter);
-                    label->setStyleSheet(labStyle);
-                    label->setFixedWidth(labWidth);
-                    layout->addWidget(label);
-                };
+                        // 定义一个函数，用来添加表头，依次传入，表头名称，以及宽度
+                        auto addNetworkItem = [&](const QString labName, const int labWidth){
+                            DLabel *label = new DLabel(labName);
+                            label->setAlignment(Qt::AlignCenter);
+                            label->setStyleSheet(labStyle);
+                            label->setFixedWidth(labWidth);
+                            layout->addWidget(label);
+                        };
 
-                // 获取网络id
-                QString id = obj.value("Id").toString().left(10);
-                addNetworkItem(id, labWidth);
+                        // 获取网络id
+                        QString id = obj.value("id").toString().left(10);
+                        addNetworkItem(id, labWidth);
 
-                // 获取网络名称
-                QString name = obj.value("Name").toString();
-                addNetworkItem(name, labWidth);
+                        // 获取网络名称
+                        QString name = obj.value("name").toString();
+                        addNetworkItem(name, labWidth);
 
-                // 获取网络名称
-                QString driver = obj.value("Driver").toString();
-                if (!driver.isEmpty())
-                {
-                    if (driver == "null")
-                    {
-                        driver = "(无)";
-                    } else if (driver == "host") {
-                        driver = "主机";
-                    } else if (driver == "bridge") {
-                        driver = "网桥";
+                        // 获取网络名称
+                        QString driver = obj.value("driver").toString();
+                        if (!driver.isEmpty())
+                        {
+                            if (driver == "null")
+                            {
+                                driver = "(无)";
+                            } else if (driver == "host") {
+                                driver = "主机";
+                            } else if (driver == "bridge") {
+                                driver = "网桥";
+                            }
+
+                        }
+                        addNetworkItem(driver, labWidth);
+
+                        // 获取连接的容器数量
+                        qint64 countContainer = obj.value("container_num").toInt();
+                        addNetworkItem(QString("%1").arg(countContainer), labWidth);
+
+                        // 生成操作栏
+                        QWidget *operationWidget = new QWidget(networkWdg);
+                        operationWidget->resize(50, mlist->getBtnDrm()->height());
+                        QHBoxLayout *operationLayout = new QHBoxLayout(operationWidget);
+                        operationLayout->setContentsMargins(10, 0, 0, 0);  //  设置左侧、顶部、右侧和底部边距，
+
+                        DPushButton *infoBtn = new DPushButton("信息");
+                        infoBtn->setStyleSheet("color: #FFFFFF; background-color: #67C23A; border-radius: 5; border: 0px; height: 30px; font-size:13px;");
+                        connect(infoBtn,&QPushButton::clicked,this,[=](){});
+                        operationLayout->addWidget(infoBtn);
+
+                        DPushButton *delBtn = new DPushButton("删除");
+                        delBtn->setStyleSheet("color: #FFFFFF; background-color: #F56C6C; border-radius: 5; border: 0px; height: 30px; font-size:13px;");
+                        connect(delBtn,&DPushButton::clicked,this,[=](){});
+                        operationLayout->addWidget(delBtn);
+
+                        DPushButton *operationBtn = new DPushButton("操作");
+                        operationBtn->setStyleSheet("color: #FFFFFF; background-color: #1E90FF; border-radius: 5; border: 0px; height: 30px; font-size:13px;");
+                        operationBtn->setCheckable(true);
+                        QMenu *operationMenu = new QMenu(operationWidget);
+                        QAction *action = operationMenu->addAction("item_1");
+                        connect(action ,&QAction::triggered ,this ,[=](){
+                                //里面写点击后执行的函数就行
+
+                        });
+                        operationMenu->addAction("item_2");
+                        operationMenu->addAction("item_3");
+                        operationBtn->setMenu(operationMenu);
+                        operationLayout->addWidget(operationBtn);
+                        layout->addWidget(operationWidget);
+
+                        QListWidgetItem *networkItem=new QListWidgetItem(mlist->getListWidget());
+                        networkItem->setSizeHint(QSize(40,40));
+                        networkItem->setFlags(Qt::ItemIsSelectable); // 取消选择项
+                        mlist->getListWidget()->setItemWidget(networkItem, networkWdg);  // 将networkWdg赋予networkItem
                     }
-
                 }
-                addNetworkItem(driver, labWidth);
-
-                // 获取连接的容器数量
-                QString countContainer = obj.value("Containers").toString();
-                qDebug() << countContainer;
-
-                // 生成操作栏
-                QWidget *operationWidget = new QWidget(networkWdg);
-                operationWidget->resize(operationLabWidth, mlist->getBtnDrm()->height());
-                QHBoxLayout *operationLayout = new QHBoxLayout(operationWidget);
-                operationLayout->setContentsMargins(10, 0, 0, 0);  //  设置左侧、顶部、右侧和底部边距，
-
-                DPushButton *infoBtn = new DPushButton("信息");
-                infoBtn->setStyleSheet("color: #FFFFFF; background-color: #67C23A; border-radius: 5; border: 0px; height: 30px; width: 30px; font-size:13px;");
-                infoBtn->setFixedWidth(60);
-                connect(infoBtn,&QPushButton::clicked,this,[=](){});
-                operationLayout->addWidget(infoBtn);
-
-                DPushButton *delBtn = new DPushButton("删除");
-                delBtn->setStyleSheet("color: #FFFFFF; background-color: #F56C6C; border-radius: 5; border: 0px; height: 30px; width: 30px; font-size:13px;");
-                connect(delBtn,&DPushButton::clicked,this,[=](){});
-                operationLayout->addWidget(delBtn);
-
-                DPushButton *operationBtn = new DPushButton("操作");
-                operationBtn->setStyleSheet("color: #FFFFFF; background-color: #1E90FF; border-radius: 5; border: 0px; height: 30px; width: 60px; font-size:13px;");
-                operationBtn->setCheckable(true);
-                QMenu *operationMenu = new QMenu(operationWidget);
-                QAction *action = operationMenu->addAction("item_1");
-                connect(action ,&QAction::triggered ,this ,[=](){
-                        //里面写点击后执行的函数就行
-
-                });
-                operationMenu->addAction("item_2");
-                operationMenu->addAction("item_3");
-                operationBtn->setMenu(operationMenu);
-                operationLayout->addWidget(operationBtn);
-                layout->addWidget(operationWidget);
-
-                QListWidgetItem *networkItem=new QListWidgetItem(mlist->getListWidget());
-                networkItem->setSizeHint(QSize(40,40));
-                networkItem->setFlags(Qt::ItemIsSelectable); // 取消选择项
-                mlist->getListWidget()->setItemWidget(networkItem, networkWdg);  // 将networkWdg赋予networkItem
             }
         }
+
     }
 }
 
