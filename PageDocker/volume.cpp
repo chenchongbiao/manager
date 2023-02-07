18583f324d3f232e1ea5355567778fef38eac053
bluesky <379865549@qq.com>
2023-02-07 19:41:11 +0800

    添加存储卷的UI初始化

--- PageDocker/volume.cpp
+++ PageDocker/volume.cpp
@@ -13,6 +13,15 @@ void Volume::initUI()
 
     // 初始化操作栏
     initOperationUI();
+
+    // 列名初始化
+    initColumnUI();
+
+    // 从sessionbus中获取数据
+    volumeArray = DBusClient::GetVolumeList();
+
+    // 初始化存储器列表
+    initNetworkListUI();
 }
 
 void Volume::initOperationUI()
@@ -42,7 +51,152 @@ void Volume::initOperationUI()
     opLayout->addWidget(createBtn);
 }
 
+void Volume::initColumnUI()
+{
+    QString labStyle = "font-size:15px;";
+    int labWidth = 110; // 标签的宽度
+    int operationLabWidth = 150;  // 操作部分的标签宽度
+    colWdg = new QWidget(mlist->getColDrm());
+    colLayout = new QHBoxLayout(colWdg);
+    colLayout->setContentsMargins(10, 0, 0, 0);  // 设置外边距
+    colLayout->setSpacing(0);  // 部件的间距
+
+    checkAllBtn = new QCheckBox(colWdg);
+    checkAllBtn->setFixedSize(mlist->getOpDrm()->height()-20,mlist->getOpDrm()->height());
+    connect(checkAllBtn,&QCheckBox::clicked,this,&Volume::CheckAllVolume);
+    colLayout->addWidget(checkAllBtn);
+
+    // 定义一个函数，用来添加表头，依次传入，表头名称，以及宽度
+    auto addColumnItem = [&](const QString labName, const int labWidth){
+        DLabel *label = new DLabel(labName);
+        label->setAlignment(Qt::AlignCenter);
+        label->setStyleSheet(labStyle);
+        label->setFixedWidth(labWidth);
+        colLayout->addWidget(label);
+    };
+
+    addColumnItem("名称", labWidth-50);
+    addColumnItem("驱动器类型", labWidth-30);
+    addColumnItem("挂载点", labWidth+90);
+    addColumnItem("创建时间", labWidth);
+    addColumnItem("操作", operationLabWidth);
+
+}
+
+void Volume::initNetworkListUI()
+{
+    QString labStyle = "font-size:15px;";
+    int labWidth = 110; // 标签的宽度
+    int operationLabWidth = 150;  // 操作部分的标签宽度
+
+    QJsonParseError jsonError;
+    QJsonDocument document = QJsonDocument::fromJson(volumeArray, &jsonError);  // 转化为 JSON 文档
+    if (!document.isNull() && (jsonError.error == QJsonParseError::NoError)) { // 解析未发生错误
+        if (document.isObject()) {  // JSON 文档为对象
+            QJsonObject object = document.object();  // 转化为对象
+            if (object.contains("data")) {  // 包含指定的 key
+                QJsonValue dataValue = object.value("data");  // 获取指定 key 对应的 value
+                if (dataValue.isArray()) { // JSON 文档为数组
+                    QJsonArray volumeJson = dataValue.toArray();
+                    int volSize = volumeJson.size();
+                    for (int i = 0; i < volSize; i++) {
+                        QJsonValue value = volumeJson.at(i);      // 取出单个json
+                        QJsonObject obj = value.toObject();        // 转换为object
+
+                        QWidget *volumeWdg = new QWidget(mlist->getListWidget());
+                        QHBoxLayout *layout = new QHBoxLayout(volumeWdg);
+                        layout->setMargin(0);  // 设置外边距
+
+                        QCheckBox *checkBtn = new QCheckBox(mlist->getListWidget());
+                        checkBtn->setFixedSize(mlist->getOpDrm()->height()-20,mlist->getOpDrm()->height());
+                        layout->addWidget(checkBtn);
+                        connect(checkBtn,&QCheckBox::clicked, this, &Volume::CheckVolume);
+
+                        // 定义一个函数，用来添加表头，依次传入，表头名称，以及宽度
+                        auto addNetworkItem = [&](const QString labName, const int labWidth){
+                         DLabel *label = new DLabel(labName);
+                         label->setAlignment(Qt::AlignCenter);
+                         label->setStyleSheet(labStyle);
+                         label->setFixedWidth(labWidth);
+                         layout->addWidget(label);
+                        };
+
+//                        名称",
+//                        驱动器类型
+//                        挂载点",
+//                        创建时间"
+//                        操作",
+                        // 获取存储卷名称
+                        QString name = obj.value("name").toString();
+                        addNetworkItem(name, labWidth-70);
+
+                        // 获取驱动器类型
+                        QString driver = obj.value("driver").toString();
+                        addNetworkItem(driver, labWidth-60);
+
+                        // 获取挂载点
+                        QString mountPoint = obj.value("mount_point").toString();
+                        addNetworkItem(mountPoint, labWidth+130);
+
+                        // 创建时间
+                        QString createTime = obj.value("create_time").toString().left(10);
+                        addNetworkItem(createTime, labWidth);
+
+                        // 生成操作栏
+                        QWidget *operationWidget = new QWidget(volumeWdg);
+                        operationWidget->resize(50, mlist->getOpDrm()->height());
+                        QHBoxLayout *operationLayout = new QHBoxLayout(operationWidget);
+                        operationLayout->setContentsMargins(10, 0, 0, 0);  //  设置左侧、顶部、右侧和底部边距，
+
+                        DPushButton *infoBtn = new DPushButton("信息");
+                        infoBtn->setStyleSheet("color: #FFFFFF; background-color: #67C23A; border-radius: 5; border: 0px; height: 30px; font-size:13px;");
+                        connect(infoBtn,&QPushButton::clicked,this,[=](){});
+                        operationLayout->addWidget(infoBtn);
+
+                        DPushButton *delBtn = new DPushButton("删除");
+                        delBtn->setStyleSheet("color: #FFFFFF; background-color: #F56C6C; border-radius: 5; border: 0px; height: 30px; font-size:13px;");
+                        connect(delBtn,&DPushButton::clicked,this,[=](){});
+                        operationLayout->addWidget(delBtn);
+
+                        DPushButton *operationBtn = new DPushButton("操作");
+                        operationBtn->setStyleSheet("color: #FFFFFF; background-color: #1E90FF; border-radius: 5; border: 0px; height: 30px; font-size:13px;");
+                        operationBtn->setCheckable(true);
+                        QMenu *operationMenu = new QMenu(operationWidget);
+                        QAction *action = operationMenu->addAction("item_1");
+                        connect(action ,&QAction::triggered ,this ,[=](){
+                                //里面写点击后执行的函数就行
+
+                        });
+                        operationMenu->addAction("item_2");
+                        operationMenu->addAction("item_3");
+                        operationBtn->setMenu(operationMenu);
+                        operationLayout->addWidget(operationBtn);
+                        layout->addWidget(operationWidget);
+
+                        QListWidgetItem *networkItem=new QListWidgetItem(mlist->getListWidget());
+                        networkItem->setSizeHint(QSize(40,40));
+                        networkItem->setFlags(Qt::ItemIsSelectable); // 取消选择项
+                        mlist->getListWidget()->setItemWidget(networkItem, volumeWdg);  // 将networkWdg赋予networkItem
+
+
+                    }
+                }
+            }
+        }
+    }
+}
+
 void Volume::SearchVolume()
+{
+
+}
+
+void Volume::CheckAllVolume()
+{
+
+}
+
+void Volume::CheckVolume()
 {
 
 }
--- PageDocker/volume.h
+++ PageDocker/volume.h
@@ -4,8 +4,16 @@
 #include <QWidget>
 #include <DLineEdit>
 #include <DPushButton>
+#include <QCheckBox>
+#include <DLabel>
+#include <QJsonObject>
+#include <QJsonDocument>
+#include <QJsonParseError>
+#include <QJsonArray>
+#include <QMenu>
 
 #include "mlistwidget.h"
+#include "dbusclient.h"
 
 DWIDGET_USE_NAMESPACE
 
@@ -20,10 +28,13 @@ public:
 public slots:
     void SearchVolume();                      // 搜索存储卷的点击事件
     void OpenCreateVolDialog();               // 创建存储卷的Dialog
+    void CheckAllVolume();                    // 选中所有存储卷
+    void CheckVolume();                       // 选择存储卷
 
 private:
     void initOperationUI();                   // 初始化操作栏的UI
     void initColumnUI();                      // 初始化列名的UI
+    void initNetworkListUI();                 // 初始化存储器列表
 
 private:
     MListWidget *mlist;                       // 通用界面的组件
@@ -37,6 +48,9 @@ private:
     DPushButton *searchBtn;                   // 搜索按钮
     DPushButton *createBtn;                   // 创建存储卷按钮
 
+    QByteArray volumeArray;                   // 从sessionbus中获取到的存储卷数据
+    QCheckBox *checkAllBtn;                   // 全选按钮
+    QList<QCheckBox *> checkBoxBtnList;       // 被选中的存储卷数据对应的按钮列表
 };
 
 #endif // VOLUME_H
