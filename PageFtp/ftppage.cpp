8c1cca4eed0f669d1f40a8c8dedac0f6820dd4ed
bluesky <379865549@qq.com>
2023-02-23 22:46:03 +0800

    添加Ftp管理

--- PageFtp/ftppage.cpp
+++ PageFtp/ftppage.cpp
@@ -12,3 +12,25 @@ FtpPage::~FtpPage()
 {
     delete ui;
 }
+
+void FtpPage::initUI()
+{
+    // 获取通用列表界面
+    mlist = new MListWidget(this);
+
+    // 搜索框及按钮部分初始化
+    initOperationUI();
+}
+
+void FtpPage::initOperationUI()
+{
+    // 操作按钮布局
+    opWdg = new QWidget(mlist->getOpDrm());
+    opWdg->resize(mlist->getOpDrm()->width(),mlist->getOpDrm()->height());
+    opLayout = new QHBoxLayout(opWdg);
+    opLayout->setSpacing(6);          // 部件之间的间距
+    opLayout->setContentsMargins(10, 0, 0, 0);   // 设置外边距
+    opLayout->setAlignment(Qt::AlignLeft);
+
+
+}
--- PageFtp/ftppage.h
+++ PageFtp/ftppage.h
@@ -16,9 +16,15 @@ class FtpPage : public QWidget
 public:
     explicit FtpPage(QWidget *parent = nullptr);
     ~FtpPage();
+    void initUI();                                 // 初始化UI
+private:
+    void initOperationUI();                        // 初始化操作栏
 
 private:
     Ui::FtpPage *ui;
+    MListWidget *mlist;             // 通用界面组件
+    QWidget *opWdg;                 // 操作部分的容器
+    QHBoxLayout *opLayout;          // 操作部分的布局
 };
 
 #endif // FTPPAGE_H
