c551b389cf9cef733d23b0a1537674d88c44abc9
bluesky <379865549@qq.com>
2023-03-03 15:02:18 +0800

    将QTableWidget的方法进行封装

--- common/multiselectlist.cpp
+++ common/multiselectlist.cpp
@@ -9,21 +9,18 @@ MultiSelectList::MultiSelectList(QWidget *parent)
 
 void MultiSelectList::initUI()
 {
-    QVBoxLayout *mainLayout = new QVBoxLayout(this);  // 主要布局 垂直布局
+    mainLayout = new QVBoxLayout(this);  // 主要布局 垂直布局
     DFrame *opFrame = new DFrame(this);  // 操作栏部分
 
-    searchEdit = new DLineEdit();
     table = new QTableWidget(this);
 
-    QHBoxLayout *opLayout = new QHBoxLayout(opFrame);
+    opLayout = new QHBoxLayout(opFrame);
 
-    searchEdit->setPlaceholderText("请输入容器名");
-    searchEdit->setFixedWidth(200);
     // 初始化一个复选框表头  参数依次为复选框坐标位置 控件大小 方向以及父控件
     header = new CheckBoxHeaderView(QPoint(10, 5), QSize(20, 20), Qt::Horizontal, table);
-//    table->setColumnCount(2);  // 设置列数
+    table->setColumnCount(1);  // 设置列数
 //    labelList.insert(0, "");  // 在第一个位置插入空字符串给复选框占位置，否则第一个标签会和复选框重叠
-//    table->setHorizontalHeaderLabels(QStringList() << "" << "测试");  // 设置标签
+    table->setHorizontalHeaderLabels(QStringList() << "");  // 设置标签
     table->setHorizontalHeader(header);
     table->setColumnWidth(0, 20);  // 设置第0列，复选框的宽度
     table->horizontalHeader()->setSectionResizeMode(0, QHeaderView::Fixed);  // 设置第0列，复选框的宽度固定
@@ -33,9 +30,9 @@ void MultiSelectList::initUI()
 
     table->setFixedSize(this->width(), 500);
 
-    opLayout->addWidget(searchEdit);
     opLayout->setAlignment(Qt::AlignLeft);
     opLayout->setContentsMargins(10, 0, 0, 0);
+    opLayout->setSpacing(5);
 
     mainLayout->addWidget(opFrame);
     mainLayout->addWidget(table);
@@ -46,4 +43,40 @@ void MultiSelectList::initUI()
 QTableWidget* MultiSelectList::getTable()
 {
     return table;
+}
+
+CheckBoxHeaderView* MultiSelectList::getHeader()
+{
+    return header;
+}
+
+void MultiSelectList::addOpLineEdit(DLineEdit *lineEdit)
+{
+    opLayout->addWidget(lineEdit);
+}
+
+void MultiSelectList::addOpButton(DPushButton *button)
+{
+    opLayout->addWidget(button);
+}
+
+void MultiSelectList::setColumnCount(int count)
+{
+     table->setColumnCount(count + 1);  // 设置列数
+}
+
+void MultiSelectList::setHeaderLabels(QStringList labelList)
+{
+    labelList.insert(0, "");  // 在第一个位置插入空字符串给复选框占位置，否则第一个标签会和复选框重叠
+    table->setHorizontalHeaderLabels(labelList);  // 设置标签
+}
+
+void MultiSelectList::setHeader(QHeaderView *header)
+{
+    table->setHorizontalHeader(header);
+}
+
+void MultiSelectList::setRowCount(int count)
+{
+    table->setRowCount(count);  // 设置数据条数
 }
--- common/multiselectlist.h
+++ common/multiselectlist.h
@@ -7,6 +7,7 @@
 #include <DFrame>
 #include <QTableWidget>
 #include <QPoint>
+#include <DPushButton>
 
 #include "checkboxheaderview.h"
 
@@ -23,14 +24,24 @@ public:
     explicit MultiSelectList(QWidget *parent = nullptr);
 
     QTableWidget* getTable();  // 获取表格控件的指针
+    CheckBoxHeaderView* getHeader();    // 获取表头指针
+    void addOpLineEdit(DLineEdit *lineEdit);   // 在操作栏添加一个DLineEdit控件
+    void addOpButton(DPushButton *button);  // 在操作栏添加一个按钮
+
+    void setColumnCount(int count);  // 设置列数
+    void setHeaderLabels(QStringList labelList);  // 设置标签
+    void setHeader(QHeaderView *header);  // 设置表头
+    void setRowCount(int count);          // 设置数据条数
 
 private:
     void initUI();            // 初始化UI
 
 private:
-    DLineEdit *searchEdit;       // 搜索框
     CheckBoxHeaderView *header;  // 带复选框的表头
     QTableWidget *table;         // 表格
+
+    QVBoxLayout *mainLayout;     // 界面的主要布局
+    QHBoxLayout *opLayout;       // 操作栏的布局
 };
 
 #endif // MULTISELECTLIST_H
