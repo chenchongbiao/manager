07fb50aabdb7abdec73a419ef644a0acbca76e43
bluesky <379865549@qq.com>
2023-03-14 22:11:30 +0800

    添加数据库接口

---
+++ PageDatabase/dbinterface.cpp
@@ -0,0 +1,6 @@
+#include "dbinterface.h"
+
+DbInterface::DbInterface()
+{
+
+}
---
+++ PageDatabase/dbinterface.h
@@ -0,0 +1,11 @@
+#ifndef DBINTERFACE_H
+#define DBINTERFACE_H
+
+
+class DbInterface
+{
+public:
+    explicit DbInterface();
+};
+
+#endif // DBINTERFACE_H
