861efa4b38454ac0c81c0dcd3894dac774c89a16
bluesky <379865549@qq.com>
2023-03-15 00:14:20 +0800

    tablewidget添加一下设置 实际效果看后续测试

--- common/tableview.cpp
+++ common/tableview.cpp
@@ -3,6 +3,8 @@
 #include <QWidget>
 #include <QPushButton>
 
+// Qt 之 QTableView 添加复选框（QAbstractTableModel） https://blog.csdn.net/liang19890820/article/details/50718340
+// QTableView详解 https://blog.csdn.net/wzz953200463/article/details/123645541
 TableView::TableView(int columnCount, QStringList labelList, const int rowCount, QWidget *parent) :
     QWidget(parent),
     table(new QTableWidget(this))
@@ -12,6 +14,11 @@ TableView::TableView(int columnCount, QStringList labelList, const int rowCount,
     // 初始化一个复选框表头  参数依次为复选框坐标位置 控件大小 方向以及父控件
     headerView = new CheckBoxHeaderView(QPoint(10, 5), QSize(20, 20), Qt::Horizontal, table);
 
+    table->setSelectionBehavior(QAbstractItemView::SelectRows); // 设置整行选中
+    table->horizontalHeader()->setStretchLastSection(true); //设置最后一栏自适应长度
+    table->setFocusPolicy(Qt::NoFocus);  // 去除选中虚线框
+    table->verticalHeader()->setVisible(false);  // 无边框
+    table->setEditTriggers(QAbstractItemView::NoEditTriggers);  // 设置表格不可编辑
     table->setColumnCount(columnCount + 1);  // 设置列数
     labelList.insert(0, "");  // 在第一个位置插入空字符串给复选框占位置，否则第一个标签会和复选框重叠
     table->setHorizontalHeaderLabels(labelList);  // 设置标签
